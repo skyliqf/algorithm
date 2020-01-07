1d1d25ec02def2250fb6d565b65216793fea3b01
skyliqf <liqifang1003@gmail.com>
2020-01-07 11:53:55 +0800

    init

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 template<typename T>
 /*
@@ -13,16 +14,40 @@ void bubble_sort(T *arr,int len){
         if(arr[j]>arr[j+1])
            swap(arr[j],arr[j+1]);
       }
+   } 
+   cout<<endl<<"bubble_sort"<<endl;
+   for(int i=0;i<len;i++){
+     cout<<arr[i]<<endl;
    }   
+}
+/*
+  选择排序
+  by skyliqf
+  2020.01.07
+*/
+template<typename V>
+void selection_sort(vector<V> arr){
+   for(int i=0;i<arr.size()-1;i++){
+     int min=i;
+     for(int j=i;j<arr.size();j++){
+        if(arr[j]<arr[min])
+           min=j;
+     }
+     swap(arr[i],arr[min]);
+   }
+   cout<<endl<<"selection_sort"<<endl;
+   for(int i=0;i<arr.size();i++){
+     cout<<arr[i]<<endl;
+   } 
 }
 
 int main(){
-   float arr[10]={9.4,12.7,4.5,6.7,4.7,8.7,9.8,2,7,26};
+   float arr[10]={9.4,1.7,4.5,6.7,4.7,8.7,9.8,2.4,7.1,2.6};
+   vector<float> arrv={9.4,9.6,4.5,6.7,4.7,8.7,9.8,2.8,7.9,2.6};
    int len = (int) sizeof(arr) / sizeof(*arr);
    bubble_sort(arr,len);
-   for(int i=0;i<len;i++){
-     cout<<arr[i]<<endl;
-   } 
+   selection_sort(arrv);
+
    return 0;
   
 
